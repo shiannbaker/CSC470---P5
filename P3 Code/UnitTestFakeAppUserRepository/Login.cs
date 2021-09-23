27557dfb230eed0ac4d510dea19781beef3aa24d
elijah-ness-dsu <elijah.ness@trojans.dsu.edu>
2021-09-23 16:46:31 -0500

    added UnitTestFakeAppUserRepository project and class

---
+++ P3 Code/UnitTestFakeAppUserRepository/Login.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestFakeAppUserRepository
+{
+    [TestClass]
+    public class Login
+    {
+        [TestMethod]
+        public void LoginWithCorrectCredentialsSuccessful()
+        {
+            // Arrange
+            FakeAppUserRepository EmpRepository = new FakeAppUserRepository();
+            // Act
+            //Assert
+        }
+    }
+}
